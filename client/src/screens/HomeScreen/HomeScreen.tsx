2b74e1618158c3ec6330b168c4de1168a6994062
NazarBeya <nazarbeya@gmail.com>
2024-07-05 15:18:58 +0300

    feat: marge

--- client/src/screens/HomeScreen/HomeScreen.tsx
+++ client/src/screens/HomeScreen/HomeScreen.tsx
@@ -1,20 +1,27 @@
 import React from "react";
 import { Text, View } from "react-native";
-import { LoadingButton } from "../../components/molecules";
+import { CommentList, LoadingButton } from "../../components/molecules";
 import useAuthStore from "../../store/AuthStore";
 import { logoutUser } from "../../services/AuthService";
+import { useGetCommentsQuery } from "../../queries/commentQueries/commentQueries";
 
 const HomeScreen = () => {
   const { setIsAuth } = useAuthStore();
+
+  const { isLoading, responce } = useGetCommentsQuery();
 
   const logout = () => {
     logoutUser();
     setIsAuth(false);
   };
 
+  if (isLoading) return null;
+
   return (
     <View>
       <Text>HomeScreen</Text>
+      {responce && <CommentList comments={responce} />}
+
       <LoadingButton title="ewqe" onPress={logout} />
     </View>
   );
